d6c12434a3d12e99336cfabd50971a06d18ba9ee
Sergey Zhukov <sergey@jetbrains.com>
2014-08-05 16:05:44 +0400

    Fixed exit code on stop

--- run-services.sh
+++ run-services.sh
@@ -24,7 +24,7 @@ fi
 
 echo "CONFD_PARAMS=${CONFD_PARAMS}"
 
-trap "/sbin/service crond stop; /sbin/service rsyslog stop; /sbin/service nginx stop; /sbin/service confd stop" SIGINT SIGTERM SIGHUP
+trap "/sbin/service crond stop; /sbin/service rsyslog stop; /sbin/service nginx stop; /sbin/service etcd stop; /sbin/service confd stop; killall tail; exit 0" SIGINT SIGTERM SIGHUP
 
 touch /var/log/confd /var/log/etcd
 
