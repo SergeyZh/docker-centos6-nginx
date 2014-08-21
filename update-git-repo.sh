e3bbb3a147286aa2e5edcbde3648ee4420f2d31d
Andrey Sizov <andrey.sizov@jetbrains.com>
2014-08-21 14:05:02 +0400

    Fixed env variable set. Added logs to container.log

--- update-git-repo.sh
+++ update-git-repo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 . /etc/sysconfig/etcdctl
+export ETCDCTL_PEERS=${ETCDCTL_PEERS}
 
 # Check: if no GIT repo here - no need to pull it
 if [ ! -d /etc/confd/.git ] ; then
@@ -11,9 +12,9 @@ cd /etc/confd
 git pull | grep -q -v 'up-to-date' && CHANGED=1
 
 if [ $CHANGED -eq 1 ]; then
-    echo "Git repo was changed" >> /var/log/container.log
+    echo "`date +%Y-%m-%d-%H%M%S` - Git repo was changed" >> /var/log/container.log
     if [ -x "/etc/confd/config-reload.sh" ] ; then
-       /etc/confd/config-reload.sh
+       /etc/confd/config-reload.sh >> /var/log/container.log
     fi
     /sbin/service confd restart
 
